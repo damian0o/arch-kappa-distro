4359adfc24b6b46ef2f9f08205bc0d1906261a8e
Damian Ospara <damian89m@gmail.com>
2026-03-26 17:40:15 +0100

    Fix build errors reported in issue #1
    - Switch deprecated uefi-x64.systemd-boot.* boot modes to uefi.systemd-boot
    - Remove [community] repo from pacman.conf (merged into [extra] in 2023)
    - Remove plasma-wayland-session (merged into plasma package)
    - Remove xf86-video-vmware (dropped from repos)
    - Replace neofetch with fastfetch (neofetch removed from repos)
    - Remove customize_airootfs.sh from file_permissions (archiso manages it)

--- kappa-profile/profiledef.sh
+++ kappa-profile/profiledef.sh
@@ -8,8 +8,7 @@ iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=(
-  'uefi-x64.systemd-boot.esp'
-  'uefi-x64.systemd-boot.eltorito'
+  'uefi.systemd-boot'
 )
 arch="x86_64"
 pacman_conf="pacman.conf"
@@ -18,5 +17,4 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 file_permissions=(
   ["/etc/sudoers.d/kappa-live"]="0:0:440"
   ["/usr/local/bin/kappa-install"]="0:0:755"
-  ["/customize_airootfs.sh"]="0:0:755"
 )
